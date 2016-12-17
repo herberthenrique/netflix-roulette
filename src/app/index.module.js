1da83c963606a48f2401b04a5be88a66755d57bb
Herbert Henrique <eu@herberthenrique.com>
2016-12-17 14:48:56 -0200

    Creating base for app, base for netflix service

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,10 +4,7 @@ import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
-import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
-import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
-import { NavbarDirective } from '../app/components/navbar/navbar.directive';
-import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
+import NetflixService from '../app/components/netflix-roulette/netflix.service';
 
 angular.module('netflixRoulette', ['ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'toastr'])
   .constant('malarkey', malarkey)
@@ -15,8 +12,5 @@ angular.module('netflixRoulette', ['ngCookies', 'ngTouch', 'ngSanitize', 'ngMess
   .config(config)
   .config(routerConfig)
   .run(runBlock)
-  .service('githubContributor', GithubContributorService)
-  .service('webDevTec', WebDevTecService)
+  .service('netflixService', NetflixService)
   .controller('MainController', MainController)
-  .directive('acmeNavbar', NavbarDirective)
-  .directive('acmeMalarkey', MalarkeyDirective);
